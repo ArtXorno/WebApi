9abcd8d3a9dac6ec6c81248b5076c0ccdb61efd2
ArtXorno <rabota.art2017@yandex.ru>
2020-09-25 14:04:14 +0000

    CRUD example, next SPA

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        DataBase _db;
+
+        //внедрение зависимости
+        public WeatherForecastController(DataBase db) => this._db = db;
+
+        [HttpGet]
+        public IEnumerable<string> Get() => _db;
+        
+
+        [HttpPost]
+        public void Post([FromBody] string value) => _db.Add(value);
+
+
+        [HttpGet("{index}")]
+        public string Get(int index) => _db[index];
+              
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value) => _db[id] = value;
+        
+
+        [HttpDelete("{value}")]
+        public void Delete(string value) => _db.Remove(value);
+        
+    }
+}
